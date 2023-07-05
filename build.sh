15260bdc54fcfacfead9ebb777331fc53b398c9f
Dane Miller <khadane.miller@gmail.com>
2023-07-05 09:13:27 -0400

    try using zip instead of tar

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cp -r src alternate-twitter-embed 
-tar cvzf alternate-twitter-embed.zip alternate-twitter-embed
+zip -r alternate-twitter-embed.zip alternate-twitter-embed
 rm -rf alternate-twitter-embed
