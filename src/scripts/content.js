937d7a6e48d4efb1601b570e0543f0cd6ccacaa4
Dane Miller <khadane.miller@gmail.com>
2023-10-15 19:48:18 -0400

    fix url replacement

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -2,9 +2,12 @@ document.addEventListener("copy", async (event) => {
   const shareLink = await navigator.clipboard.readText();
   // change vx to whatever mirror works
   // currently vx and fx still work
-  const blob = new Blob([shareLink.replace(/https:\/\//, "https://vx")], {
-    type: "text/plain",
-  });
+  const blob = new Blob(
+    [shareLink.replace("https://x.com", "https://fxtwitter.com")],
+    {
+      type: "text/plain",
+    }
+  );
   const data = [new ClipboardItem({ ["text/plain"]: blob })];
   await navigator.clipboard.write(data);
   event.preventDefault();
